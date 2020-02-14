cce3d738a2086fb98801dea2e3b250e00d207cc9
Musa <zuluchs@gmail.com>
2020-02-14 12:34:42 +0200

    renamed column

--- Documents/scripts/items-ordered-for-the-week.sql
+++ Documents/scripts/items-ordered-for-the-week.sql
@@ -1,6 +1,6 @@
 --B. We would like to run a report that shows us all the menu items available, with the total number of items ordered for the week.
 
-SELECT DISTINCT DishName, FoodItemDescription, Price, Count(*) as Items_Ordered FROM [FoodItems]
+SELECT DISTINCT DishName, FoodItemDescription, Price, Count(*) as Items_Ordered_This_Week FROM [FoodItems]
 LEFT JOIN [OrderItems] ON
 [OrderItems].[FoodItemId] = [FoodItems].[FoodItemId]
 LEFT JOIN [Orders] on [Orders].[OrderId] = [OrderItems].[OrderId]
